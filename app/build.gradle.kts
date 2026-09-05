3f1ec6dc98208ad4a7104282344cab5de4940fbe
ant-cave <antmmmmm@126.com>
2026-09-05 18:35:26 +0800

    chore: bump version to v1.2.0-beta.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.clearscan"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.2.0-beta.6"
+        versionCode = 11
+        versionName = "1.2.0-beta.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
             abiFilters += listOf("arm64-v8a")
