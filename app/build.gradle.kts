b5fa6db7b7767844f4cb0f26ede5909a759a0ce9
ant-cave <antmmmmm@126.com>
2026-09-06 16:40:13 +0800

    chore: bump to v1.2.0-beta.9 (versionCode 13)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.clearscan"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
-        versionName = "1.2.0-beta.8"
+        versionCode = 13
+        versionName = "1.2.0-beta.9"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
             abiFilters += listOf("arm64-v8a")
